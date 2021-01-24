bb465cd141aa862f096fda14f3fb5b46030011d4
washington <thewashington.souza@gmail.com>
2021-01-24 01:46:46 -0300

    feat: create dynamo insert for heroes.insert lambda

--- heroes/src/heroes.insert.js
+++ heroes/src/heroes.insert.js
@@ -1,18 +1,63 @@
+const AWS = require('aws-sdk')
+const uuid = require('uuid')
+
+const dynamoDB = new AWS.dynamoDB.DocumentClient()
+
 class Handler {
-  async main() {
+  constructor({ dynamoDbSvc }) {
+    this.dynamoDbSvc = dynamoDbSvc
+    this.dynamodbTable = process.env.DYNAMODB_TABLE
+  }
+
+  async insertItem(params) {
+    return this.dynamoDbSvc.put(params).promise()
+  }
+
+  prepareData(data) {
+    const params = {
+      TableName: this.dynamodbTable,
+      Item: {
+        ...data,
+        id: uuid.v1(),
+        createdAt: new Date().toISOString()
+      }
+    }
+
+    return params
+  }
+
+  handlerSuccess(data) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+
+    return response
+  }
+
+  handlerError(data) {
+    const result = {
+      statusCode: data.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: `Couldn't create item!!!`
+    }
+  }
+
+  async main(event) {
+    const data = JSON.parse(event.body)
+    const dbParams = this.prepareData(data)
+    await this.insertItem(dbParams)
+
     try {
-      return {
-        statusCode: 200,
-        body: 'Hello'
-      }
+      return this.handlerSuccess(dbParams.Item)
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: 'Internal server error'
-      }
+      console.log('Error on insert data ', error.stack)
+      return this.handlerError({ statusCode: 500 })
     }
   }
 }
 
-const handler = new Handler()
+const handler = new Handler({
+  dynamoDbSvc: dynamoDB
+})
 module.exports = handler.main.bind(handler)
