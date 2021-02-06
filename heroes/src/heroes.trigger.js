4c44aa3d6220215db567af44003d72e639a5779b
washington <thewashington.souza@gmail.com>
2021-02-06 16:26:19 -0300

    feat: implement hero-trigger lambda

---
+++ heroes/src/heroes.trigger.js
@@ -0,0 +1,19 @@
+const { MachineLearning } = require("aws-sdk");
+
+const handler = {
+  async main(event) {
+    console.log(
+      'Event***', JSON.stringify(
+        event,
+        null,
+        2
+      )
+    )
+
+    return {
+      statusCode: 200
+    }
+  }
+}
+
+module.exports = handler.main.bind(handler)
